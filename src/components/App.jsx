b093ef972bd67de704f52f2086899ca0ce39fdf3
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-01 17:31:08 +0200

    add 1 part 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import { Feedback } from './Feedback/Feedback';
 import { createGlobalStyle } from 'styled-components';
+
 const GlobalStyle = createGlobalStyle`
-  ul,h1,h2,h3,h4,h5,h6,li{list-style:none;margin:0;padding:0;};body{
-        display: flex;
-        justify-content:center;
-        align-items:center;
-        color: '#010101';
-        
+  ul,h1,h2,h3,h4,h5,h6,li{list-style:none;margin:0;padding:0;};
+  body{
+   display: flex;
+   justify-content:center;
+   align-items:center;
+   color: '#010101'; 
   }
 `;
-
 export const App = () => {
   return (
     <div>
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -11,20 +11,36 @@ export class Feedback extends Component {
     neutral: 0,
     bad: 0,
   };
+  onGoodClick = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+  onNeutralClick = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+  onBadClick = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+
   render() {
     return (
       <div>
         <FeedbackText>Please, leave feedback</FeedbackText>
         <FeedbackBtnWrapper>
-          <FeedbackBtn>Good</FeedbackBtn>
-          <FeedbackBtn>Neutral</FeedbackBtn>
-          <FeedbackBtn>Bad</FeedbackBtn>
+          <FeedbackBtn onClick={this.onGoodClick}>Good</FeedbackBtn>
+          <FeedbackBtn onClick={this.onNeutralClick}>Neutral</FeedbackBtn>
+          <FeedbackBtn onClick={this.onBadClick}>Bad</FeedbackBtn>
         </FeedbackBtnWrapper>
         <FeedbackText>Statistics</FeedbackText>
         <FeedbackList>
-          <li>Good</li>
-          <li>Neutral</li>
-          <li>Bad</li>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
         </FeedbackList>
       </div>
     );
